e77e2814f153c3e436e281a737887766247b27fa
LuisDubioza <luisriv@ciencias.unam.mx>
2025-08-21 00:47:09 -0600

    creo que agrege la practica

---
+++ src/Practica1.java
@@ -0,0 +1,9 @@
+public class Practica1 {
+    public static void main(String[] args) {
+        System.out.println("                             Muchas felicidades Luis!! \n" // No fui a la primer semana y no tengo equipo :'(
+                + "En la facultad estamos que no cabemos de gusto y orgullo porque estás aprendiendo \n"
+                + "                      a programar en Java con orientación a objetos.\n"
+                + "                           De verdad \"Muchas felicidades!!\" \n"
+                + "                                     Un abrazo :) \n");
+    }
+}
---
+++ src/Practica1Parte2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Practica1Parte2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println( "Bienvenido a calculadora de conversión de libra a kilo \n"
+        + "Ingresa el monto en libras a convertir \n" );
+        double libras = scanner.nextDouble();
+
+        System.out.println(libras);
+    }
+
+
+}
