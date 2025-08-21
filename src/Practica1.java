52588ab8b8b440dd26217f8f391b9d5a49db2dbb
LuisDubioza <luisriv@ciencias.unam.mx>
2025-08-21 01:56:52 -0600

    corregi ortografia

--- src/Practica1.java
+++ src/Practica1.java
@@ -9,7 +9,7 @@ public class Practica1 {
         String n = scanner.nextLine();
         System.out.println("\n");
 
-        System.out.println( "Muchas felicidades " + n + "\n");
+        System.out.println( "Muchas felicidades " + n + "!!\n");
         System.out.println("En la facultad estamos que no cabemos de gusto y orgullo por que están aprendiendo \n");
         System.out.println("a programar en Java con orientación a objetos. \n");
         System.out.println("De verdad \"Muchas felicidades!!\" \n");
